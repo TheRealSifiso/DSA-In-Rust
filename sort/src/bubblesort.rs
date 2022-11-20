c9330de5d07a528d314355891c19662a70ea2ba1
Sifiso <sifiso1101@gmail.com>
2022-11-20 16:38:05 +0200

    Move Bubble Sort test

--- sort/src/bubblesort.rs
+++ sort/src/bubblesort.rs
@@ -8,15 +8,26 @@ impl Sorter for BubbleSort {
         let mut swapped = true;
 
         while swapped {
+
             swapped = false;
 
-            for i in 0..slice.len() - 1 {
-                if slice[i] > slice[i + 1]{
+            for i in 0..slice.len() - 1{
+                if slice[i] > slice[i + 1] {
                     slice.swap(i, i + 1);
                     swapped = true;
                 }
             }
+            
         }
 
     }
+}
+
+#[test]
+fn bubble_sort_works(){
+    let mut things = vec![9, 8, 3 , 5, 0, 7];
+
+    super::sort::<_, BubbleSort>(&mut things);
+    assert_eq!(things, &[0, 3, 5, 7, 8, 9])
+
 }
