eeb7e5fe3c66d7271f94cf2430817278566f5b94
unknown <sifiso1101@gmail.com>
2022-12-09 22:03:02 +0200

    Add unsafe implementation for iter_mut

--- iterators/src/lib.rs
+++ iterators/src/lib.rs
@@ -8,15 +8,15 @@ mod tests {
     #[test]
     fn it_works(){
 
-        let x = NewType(vec![1, 2, 3, 4, 5]);
+        let mut x = NewType(vec![1, 2, 3, 4, 5]);
 
         let mut collection: Vec<&i32> = Vec::new();
 
-        for item in x.iter() {
+        for item in x.iter_mut() {
             collection.push(item);
         }
 
-        assert_eq!(vec![&1, &2, &3, &4, &5], collection);
+        assert_eq!(vec![&mut 1, &mut 2, &mut 3, &mut 4, &mut 5], collection);
 
     }
     
--- iterators/src/newtypeiterator.rs
+++ iterators/src/newtypeiterator.rs
@@ -1,27 +1,61 @@
 pub struct NewType<T>(pub Vec<T>);
 
 impl<T> NewType<T> {
-    pub fn iter(&self) -> NewTypeIter<'_, T>{
-        NewTypeIter { inner: self, pos: 0 }
+    pub fn iter(&self) -> NewTypeIter<'_, T> {
+        NewTypeIter {
+            inner: self,
+            pos: 0,
+        }
+    }
+
+    pub fn iter_mut(&mut self) -> NewTypeIterMut<'_, T> {
+        NewTypeIterMut {
+            inner: self,
+            pos: 0,
+        }
     }
 }
 
-pub struct NewTypeIter<'a, T> {
+pub struct NewTypeIter<'a, T: 'a> {
     inner: &'a NewType<T>,
     pos: usize,
 }
 
-impl<'a, T> Iterator for NewTypeIter<'a, T>{
+impl<'a, T> Iterator for NewTypeIter<'a, T> {
     type Item = &'a T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        
         if self.pos >= self.inner.0.len() {
             return None;
         }
 
-        self.pos +=1;
+        self.pos += 1;
         self.inner.0.get(self.pos - 1)
+    }
+}
 
+pub struct NewTypeIterMut<'a, T> {
+    inner: &'a mut NewType<T>,
+    pos: usize,
+}
+
+impl<'a, T> Iterator for NewTypeIterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&'_ mut self) -> Option<Self::Item> {
+        if self.pos >= self.inner.0.len() {
+            return None;
+        }
+
+        self.pos += 1;
+        let innerT = self.inner.0.get_mut(self.pos - 1).unwrap();
+
+        let ptr = innerT as *mut T;
+
+        unsafe{
+            ptr.as_mut()
+        }
+
+        //todo!();
     }
 }
