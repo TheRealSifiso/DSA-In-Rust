4b1d697a61b605032ec23749c10f3a7245078a44
unknown <sifiso1101@gmail.com>
2022-12-09 16:11:35 +0200

    Implement iterator for NewTypeIter

--- iterators/src/newtypeiterator.rs
+++ iterators/src/newtypeiterator.rs
@@ -1,2 +1,27 @@
 struct NewType<T>(Vec<T>);
 
+impl<T> NewType<T> {
+    fn iter(&self) -> NewTypeIter<'_, T>{
+        NewTypeIter { inner: self, pos: 0 }
+    }
+}
+
+struct NewTypeIter<'a, T> {
+    inner: &'a NewType<T>,
+    pos: usize,
+}
+
+impl<'a, T> Iterator for NewTypeIter<'a, T>{
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        
+        if self.pos >= self.inner.0.len() {
+            return None;
+        }
+
+        self.pos +=1;
+        self.inner.0.get(self.pos - 1)
+
+    }
+}
